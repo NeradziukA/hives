b2dee80bc1f8ab9a05f7110b7d7de4d8c05891c3
Aliaksandr Neradziuk <aliaksandr_neradziuk@epam.com>
2025-03-18 11:49:23 +0100

    client init

---
+++ messaging/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcupHbNdg3S4yURuHKJMY-an3Zj0G-2wE",
+  authDomain: "hives-c6f96.firebaseapp.com",
+  projectId: "hives-c6f96",
+  storageBucket: "hives-c6f96.firebasestorage.app",
+  messagingSenderId: "597680517665",
+  appId: "1:597680517665:web:60be97a0efe4831d646960"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
