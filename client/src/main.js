94102f443768c3b0d373e963aadb07bc42a53e25
Aliaksandr Neradziuk <aliaksandr_neradziuk@epam.com>
2025-03-19 16:03:36 +0100

    add model

--- client/src/main.js
+++ client/src/main.js
@@ -2,36 +2,58 @@ import * as THREE from "three";
 import { renderer } from "./renderer";
 import { UnitModel } from "./models";
 import { Coords } from "../../lib/geo/coords";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+const loader = new GLTFLoader();
+
+loader.load(
+  "funko_test_model.glb",
+
+  function (gltf) {
+    const unit = gltf.scene;
+    unit.scale.set(0.02, 0.02, 0.02);
+    unit.position.x = 0;
+    unit.position.y = 0;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    scene.add(unit);
+
+    const unit1 = new UnitModel();
+    unit1.moveTo(new Coords(2, 0));
+    scene.add(unit1.renderObj);
+
+    const unit2 = new UnitModel();
+    unit2.moveTo(new Coords(-2, 0));
+    scene.add(unit2.renderObj);
+
+    renderer.setAnimationLoop(animate);
+
+    camera.position.z = 10;
+
+    function animate() {
+      const rotation = unit1.renderObj.rotation;
+      const rotation2 = unit2.renderObj.rotation;
+      rotation.x += 0.01;
+      rotation.y += 0.01;
+      rotation2.x += 0.02;
+      rotation2.y += 0.01;
+      unit.rotation.y += 0.02;
+
+      renderer.render(scene, camera);
+    }
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
 );
-const unit = new UnitModel();
-renderer.setAnimationLoop(animate);
-unit.moveTo(new Coords(2, 0));
-
-scene.add(unit.renderObj);
-
-const unit2 = new UnitModel();
-
-unit2.moveTo(new Coords(-2, 0));
-
-scene.add(unit2.renderObj);
-
-renderer.setAnimationLoop(animate);
-
-camera.position.z = 10;
-
-function animate() {
-  const rotation = unit.renderObj.rotation;
-  const rotation2 = unit2.renderObj.rotation;
-  rotation.x += 0.01;
-  rotation.y += 0.01;
-  rotation2.x += 0.02;
-  rotation2.y += 0.01;
-  renderer.render(scene, camera);
-}
